19297bbc6313427381910bbe7b3fb9baf65ab1e6
Adnan <amail.moc@gmail.com>
2021-01-24 12:39:28 +0330

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const server = http.createServer(myApp);
 var io = require('socket.io')(server);
 
 io.on('connection',function(socket){
-  socket.on('g-upload',handi.upload.bind(socket));
-  socket.on('g-start',handi.start.bind(socket));  
-  socket.on('archive',handi.archive.bind(socket))
+  socket.on('g-upload',handi.upload.call(socket));
+  socket.on('g-start',handi.start.call(socket));  
+  socket.on('archive',handi.archive.call(socket))
 })
 
 server.listen(3000);
